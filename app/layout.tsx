b1b0c6dccbbeb1994e9e7aeea44088d7a6e85a1c
shengping <aa469@example.com>
2025-12-31 21:22:18 +0800

    Fix provider import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,12 @@
 import { config } from "@/config";
-import { AlchemyAccountComponentsProvider } from "@account-kit/react";
+import { AlchemyAccountProvider } from "@account-kit/react";
 import "@account-kit/react/styles.css"; // 這行絕對不能漏，否則會沒樣式
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
-        <AlchemyAccountComponentsProvider config={config}>
+       <AlchemyAccountProvider config={config}>
           {children}
         </AlchemyAccountComponentsProvider>
       </body>
